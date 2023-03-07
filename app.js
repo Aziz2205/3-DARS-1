9d89a87a25a7e17b872fdb6157707ddf42dcc4f9
azizbekumarzhonov <aziz22052007@gmail.com>
2023-03-07 22:34:30 +0300

    fulls

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 let son = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-let submit = document.getElementById("btn")
+let submit = document.getElementById("submit")
+let rang = document.querySelector(".color")
 
-function rndm() {
-    return Math.floor(Math.random() * son.length);
-}
-
-submit.addEventListener("click", () => {
+submit.addEventListener("click", function () {
     let numClr = "#";
     for (i = 0; i < 6 ; i++ ){
         numClr += son[rndm()];
     }
+    rang.textContent = numClr;
+    document.body.style.backgroundColor = numClr;
 });
+
+function rndm() {
+    return Math.floor(Math.random() * son.length);
+}
